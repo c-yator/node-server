d044208b89a7954e2a2bacf29a44b3c50c4d03fb
christian yator <christianyator7@gmail.com>
2020-09-23 11:45:41 +0300

    modified auth response

--- Config/jwtConfig.js
+++ Config/jwtConfig.js
@@ -8,7 +8,7 @@ module.exports = {
 			const payload = {};
 			const secret = process.env.ACCESS_TOKEN_SECRET;
 			const options = {
-				expiresIn: '15m',
+				expiresIn: '30s',
 				issuer: 'yotefresh.com',
 				audience: userId,
 			};
