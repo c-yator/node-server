0541cd18fd71cd609c6b08235ca63c448ace9cd5
cyator <christianyator7@gmail.com>
2020-11-28 21:52:14 +0300

    joi update

--- Config/validationSchema.js
+++ Config/validationSchema.js
@@ -1,7 +1,7 @@
 const joi = require('joi');
 
 const registerSchema = joi.object({
-	username: joi.string().min(3).max(30),
+	username: joi.string().min(2).max(30),
 	email: joi.string().email().lowercase().required(),
 	password: joi.string().min(8).max(30).required(),
 });
