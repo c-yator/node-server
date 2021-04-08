ca5c98976d558f2cd21601bd610feaf938cf6445
c-yator <christian.yator@students.jkuat.ac.ke>
2021-04-08 16:31:40 +0300

    redis config

--- Config/init_redis.js
+++ Config/init_redis.js
@@ -1,7 +1,7 @@
 const redis = require('redis');
 const client = redis.createClient({
 	port: 6379,
-	host: '127.0.0.1',
+	host: '192.168.1.122',
 });
 client.on('connect', () => {
 	console.log('client connected to redis');
